7a67eec3840190e4ef770c2bc84736e741bf7fe1
snippingaddict <nikolay-96@hotmail.com>
2020-06-25 22:03:01 +0200

    Dodat header i footer

--- src/app/shared/browser/footer/footer.component.ts
+++ src/app/shared/browser/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
@@ -7,7 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ){
+    this.matIconRegistry.addSvgIcon(
+      "facebook",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/svg/facebook.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "twitter",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/svg/twitter.svg")
+    );
+    this.matIconRegistry.addSvgIcon(
+      "googleplus",
+      this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/svg/google+.svg")
+    );
+  }
 
   ngOnInit(): void {
   }
